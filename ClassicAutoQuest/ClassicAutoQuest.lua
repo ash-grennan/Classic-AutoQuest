b3c117cfc307f82071421149381fe630df452f95
Ash Grennan <ashgrennan@gmail.com>
2019-08-11 14:06:14 +0100

    feat(gossip): now skips past gossip pre quest hand in

--- ClassicAutoQuest/ClassicAutoQuest.lua
+++ ClassicAutoQuest/ClassicAutoQuest.lua
@@ -2,21 +2,31 @@
 -- Name: ClassicAutoQuest
 -- Author: BobNudd
 -- Description: A minimalist auto quest acceptor/hand-in.
--- Version: 1.1.0
+-- Version: 1.2.0
 
 
 
-local detail = CreateFrame("Frame")
-detail:RegisterEvent("QUEST_DETAIL")
-detail:SetScript("OnEvent", function()
+local qProgress = CreateFrame("Frame")
+qProgress:RegisterEvent("QUEST_PROGRESS")
+qProgress:SetScript("OnEvent", function()
+	 if (IsQuestCompletable()) then
+		CompleteQuest();
+	 end
+end)
+
+
+
+local qDetail = CreateFrame("Frame")
+qDetail:RegisterEvent("QUEST_DETAIL")
+qDetail:SetScript("OnEvent", function()
 	 AcceptQuest();
 end)
 
 
-local gossip = CreateFrame("Frame")
-gossip:RegisterEvent("GOSSIP_SHOW")
-gossip:SetScript("OnEvent", function()
-	if GetNumGossipAvailableQuests() > 0 then
+local qGossip = CreateFrame("Frame")
+qGossip:RegisterEvent("GOSSIP_SHOW")
+qGossip:SetScript("OnEvent", function()
+	if (GetNumGossipAvailableQuests() > 0) then
 		local arg = { GetGossipAvailableQuests() }
 		local i = 1
 		while(arg[i]) do
@@ -24,23 +34,35 @@ gossip:SetScript("OnEvent", function()
 			i = i + 1;
 		end
 	end
+	if (GetNumGossipActiveQuests() > 0) then
+		local cnt = GetNumGossipActiveQuests();
+		for i = 1, cnt do
+			SelectGossipActiveQuest(i); 
+			_QuestChoices();
+			i = i + 1;
+		end
+	end
 end)
 
 
-local complete = CreateFrame("Frame")
-complete:RegisterEvent("QUEST_COMPLETE")
-complete:SetScript("OnEvent", function()
-		if (GetQuestMoneyToGet() > 0) then
-			print("AQH: Quest requires monies to complete");
-		end
-		if (GetNumQuestChoices() == 0) then
-			QuestFrameCompleteButton:Click();
-			QuestFrameCompleteQuestButton:Click();
-		end
-		if (GetNumQuestChoices() == 1) then
-			QuestInfoFrame.itemChoice = 1; 
-			GetQuestReward(QuestInfoFrame.itemChoice);
-			QuestFrameCompleteButton:Click();
-		end
+local qComplete = CreateFrame("Frame")
+qComplete:RegisterEvent("QUEST_COMPLETE")
+qComplete:SetScript("OnEvent", function()
+	_QuestChoices();
 end)
 
+
+function _QuestChoices()
+	if (GetQuestMoneyToGet() > 0) then
+		print("AQH: Quest requires monies to complete");
+	end
+	if (GetNumQuestChoices() == 0) then
+		QuestFrameCompleteButton:Click();
+		QuestFrameCompleteQuestButton:Click();
+	end
+	if (GetNumQuestChoices() == 1) then
+		QuestInfoFrame.itemChoice = 1; 
+		GetQuestReward(QuestInfoFrame.itemChoice);
+		QuestFrameCompleteButton:Click();
+	end
+end
