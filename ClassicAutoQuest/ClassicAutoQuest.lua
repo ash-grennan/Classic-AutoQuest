db57aaf6fe3734752a2a042835b69d006f0b61c1
Ash Grennan <ashgrennan@gmail.com>
2019-08-15 20:50:41 +0100

    feat(itemlink): upon accepting a quests, rewards are linked in chat

--- ClassicAutoQuest/ClassicAutoQuest.lua
+++ ClassicAutoQuest/ClassicAutoQuest.lua
@@ -2,10 +2,7 @@
 -- Name: ClassicAutoQuest
 -- Author: BobNudd
 -- Description: A minimalist auto quest acceptor/hand-in.
--- Version: 1.3.0
-
-
-
+-- Version: 1.4.0
 
 local qProgress = CreateFrame("Frame")
 qProgress:RegisterEvent("QUEST_PROGRESS")
@@ -34,8 +31,26 @@ end)
 local qDetail = CreateFrame("Frame")
 qDetail:RegisterEvent("QUEST_DETAIL")
 qDetail:SetScript("OnEvent", function()
-	 AcceptQuest();
+	local num = GetNumQuestChoices();
+	AcceptQuest();
+	if (num <= 0) then
+		return
+	end
+	for x = 1, num do
+		__QuestReward(x);
+	end
 end)
+
+
+ function __QuestReward(index)
+	local link = GetQuestItemLink("choice", index);
+ 	if (not link) then
+		 _qItemLookup("QuestReward",1 ,function(self) return __QuestReward() end)
+	else
+		print("|cFF00FF00 AQ: Quest reward : ", link);
+	end
+ end
+
 
 
 local qGossip = CreateFrame("Frame")
@@ -80,4 +95,31 @@ function _QuestChoices()
 		GetQuestReward(QuestInfoFrame.itemChoice);
 		QuestFrameCompleteButton:Click();
 	end
+end
+
+
+
+function _qItemLookup(TimerName,Time,funct,...)
+	local framename = "TimeFrame"..TimerName
+	if _G[framename] and (_G[framename]:GetScript("OnUpdate") ~= nil) then return end
+
+	local frame = CreateFrame("Frame", framename)
+	local totalElapsed = 0.0
+	local tickcount = 0
+	local vars = {...}
+	
+	local function LibKjasiTimer_onUpdate(self, elapsed)
+		totalElapsed = totalElapsed + elapsed
+		if (totalElapsed < 1) then return end
+		totalElapsed = totalElapsed - floor(totalElapsed)
+		tickcount = tickcount + 1
+
+		if (tickcount == Time) then
+			local result = funct(vars)
+			frame:SetScript("OnUpdate", nil)
+			return result
+		end
+	end
+	frame:SetScript("OnUpdate", LibKjasiTimer_onUpdate)
+	frame:Show()
 end
